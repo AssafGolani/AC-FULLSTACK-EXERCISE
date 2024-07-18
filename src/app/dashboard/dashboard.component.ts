c6a556133962da986391d8e8145db165774e53b4
Assaf <1214assaf@gmail.com>
2024-07-18 12:15:25 +0300

    feat: Add person data retrieval to dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HeartRateData, Person } from '../models/person.model';
+import { AccessDbService } from '../services/access-db.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private accessDbService: AccessDbService = inject(AccessDbService);
+  private authService: AuthService = inject(AuthService);
+  people: Person[] = [];
+  selectedPerson: Person | null = null;
+  heartRateData: HeartRateData | null = null;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.accessDbService.getPersonData().subscribe((people) => {
+      this.people = people;
+    });
+  }
+
+  selectPerson(person: Person): void {
+    this.selectedPerson = person;
+    this.heartRateData = null;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
