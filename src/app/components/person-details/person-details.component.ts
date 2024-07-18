ccce03fde54545190180ab471cf743e35522db6e
Assaf <1214assaf@gmail.com>
2024-07-18 20:19:59 +0300

    refactor: Update person-details component to use separate person-info and heart-rate-info components

--- src/app/components/person-details/person-details.component.ts
+++ src/app/components/person-details/person-details.component.ts
@@ -2,11 +2,18 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { HeartRateData, Person } from '../../models/person.model';
+import { HeartRateInfoComponent } from '../heart-rate-info/heart-rate-info.component';
+import { PersonInfoComponent } from '../person-info/person-info.component';
 
 @Component({
   selector: 'app-person-details',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    PersonInfoComponent,
+    HeartRateInfoComponent,
+  ],
   templateUrl: './person-details.component.html',
   styleUrl: './person-details.component.css',
 })
