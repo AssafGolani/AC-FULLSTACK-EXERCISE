7c0aa35cfadb41e9987c5cefc14d2c795a3b0ccb
Assaf <1214assaf@gmail.com>
2024-07-18 18:51:52 +0300

    refactor: Sort person list based on selected criteria

--- src/app/person-list/person-list.component.ts
+++ src/app/person-list/person-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { Person } from '../models/person.model';
@@ -12,9 +18,29 @@ import { Person } from '../models/person.model';
 })
 export class PersonListComponent {
   @Input() people: Person[] | null = [];
+  @Input() sortBy: string | null = null;
   @Output() personSelected = new EventEmitter<Person>();
+
+  sortedPeople: Person[] | null = [];
 
   selectPerson(person: Person) {
     this.personSelected.emit(person);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['people'] || changes['sortBy']) {
+      this.sortPeople();
+    }
+  }
+
+  sortPeople() {
+    if (this.people) {
+      this.sortedPeople = [...this.people];
+      if (this.sortBy === 'lastName') {
+        this.sortedPeople.sort((a, b) => a.LastName.localeCompare(b.LastName));
+      } else if (this.sortBy === 'age') {
+        this.sortedPeople.sort((a, b) => a.Age - b.Age);
+      }
+    }
   }
 }
