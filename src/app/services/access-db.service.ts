624c87a5a6b052e3ee3080d247b35851e6d09111
Assaf <1214assaf@gmail.com>
2024-07-18 14:40:40 +0300

    refactor: Add calculateHeartRateData and calculateBMI methods to AccessDbService

--- src/app/services/access-db.service.ts
+++ src/app/services/access-db.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Person } from '../models/person.model';
+import { HeartRateData, Person } from '../models/person.model';
+import { Status } from '../enums/status.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,41 @@ export class AccessDbService {
         'x-apikey': 'f534b6365ca29a799b267c496ba312d4247d9',
       },
     });
+  }
+
+  calculateHeartRateData(heartRateString: string): HeartRateData {
+    const heartRates = heartRateString
+      .split(';')
+      .map((rate) => parseInt(rate, 10));
+    let status: Status = Status.Sleeping;
+    let firstMinuteSleeping: number | null = null;
+    let firstMinuteAwake: number | null = null;
+    let firstMinuteWorkout: number | null = null;
+    let totalHeartRate = 0;
+
+    heartRates.forEach((rate, index) => {
+      totalHeartRate += rate;
+      if (rate < 70 && status !== Status.Sleeping) {
+        status = Status.Sleeping;
+        firstMinuteSleeping = firstMinuteSleeping || index + 1;
+      } else if (rate >= 70 && rate < 90 && status !== Status.Awake) {
+        status = Status.Awake;
+        firstMinuteAwake = firstMinuteAwake || index + 1;
+      } else if (rate >= 90 && status !== Status.Workout) {
+        status = Status.Workout;
+        firstMinuteWorkout = firstMinuteWorkout || index + 1;
+      }
+    });
+
+    return {
+      firstMinuteSleeping,
+      firstMinuteAwake,
+      firstMinuteWorkout,
+      averageHeartRate: totalHeartRate / heartRates.length,
+    };
+  }
+
+  calculateBMI(weight: number, height: number): number {
+    return weight / (height / 100) ** 2;
   }
 }
