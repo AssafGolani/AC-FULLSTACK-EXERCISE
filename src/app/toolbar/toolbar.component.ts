da59a79d6ae11a476b72665e6e077a6446d17015
Assaf <1214assaf@gmail.com>
2024-07-18 14:22:05 +0300

    refactor: Update toolbar component to include welcome text for logged in users

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
@@ -11,8 +11,15 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
   private authService: AuthService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn$.subscribe((isLoggedIn) => {
+      this.isLoggedIn = isLoggedIn;
+    });
+  }
 
   logout(): void {
     this.authService.logout();
