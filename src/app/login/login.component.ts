df56dc6339a5dfd035e089abf35fbd06156c11f4
Assaf <1214assaf@gmail.com>
2024-07-18 11:57:25 +0300

    feat: Add common modules, form components, and services to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private fb: FormBuilder = inject(FormBuilder);
+  private router: Router = inject(Router);
+  private authService: AuthService = inject(AuthService);
+  loginForm: FormGroup;
+  attemptsCounter = 0;
+  lockoutTime = 0;
 
+  constructor() {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {}
+
+  get isLockedOut(): boolean {
+    return this.lockoutTime > Date.now();
+  }
+
+  get lockoutRemaining(): number {
+    return Math.max(0, Math.ceil((this.lockoutTime - Date.now()) / 1000));
+  }
 }
