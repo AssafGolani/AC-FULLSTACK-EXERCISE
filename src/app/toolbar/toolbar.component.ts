879b5735f4d15a38cb551a213aa3d89e26da8ed4
Assaf <1214assaf@gmail.com>
2024-07-18 13:52:46 +0300

    refactor: Update toolbar component to include material modules and inject AuthService

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.css'
+  styleUrl: './toolbar.component.css',
 })
 export class ToolbarComponent {
+  private authService: AuthService = inject(AuthService);
 
+  logout(): void {
+    this.authService.logout();
+  }
 }
