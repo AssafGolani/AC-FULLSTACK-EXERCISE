9b8d5d9ef2b6a791fea0e18c41d691592dd3014a
Assaf <1214assaf@gmail.com>
2024-07-18 14:06:33 +0300

    refactor: Update person-details component to display person details with heart rate data and BMI

--- src/app/person-details/person-details.component.ts
+++ src/app/person-details/person-details.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { HeartRateData, Person } from '../models/person.model';
 
 @Component({
   selector: 'app-person-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './person-details.component.html',
-  styleUrl: './person-details.component.css'
+  styleUrl: './person-details.component.css',
 })
 export class PersonDetailsComponent {
-
+  @Input() person: Person | null = null;
+  @Input() heartRateData: HeartRateData | null = null;
+  @Input() bmi: number | null = null;
 }
