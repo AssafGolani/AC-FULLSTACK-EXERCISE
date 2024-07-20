16066b2d80fc4c308eb512277d0a95cc751c0dce
Assaf <1214assaf@gmail.com>
2024-07-20 10:10:35 +0300

    refactor: Update API URL in environment.ts file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: '/api', // This uses the proxy configuration during development
+  apiUrl: 'https://healthrate-6cb6.restdb.io', // This uses the proxy configuration during development
   apiKey: '669a66ec70346f45ff068723',
 };
