e63210cc8172464f1a2d7e7e8ff90088685ff7b0
Assaf <1214assaf@gmail.com>
2024-07-20 11:08:59 +0300

    refactor: Update skeleton loader component styles and add loading state to dashboard

--- src/app/common/skeleton-loader/skeleton-loader.component.ts
+++ src/app/common/skeleton-loader/skeleton-loader.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-skeleton-loader',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressBarModule],
   templateUrl: './skeleton-loader.component.html',
   styleUrl: './skeleton-loader.component.css',
 })
